d92b10061eec6034d2d53ca0d707f8d096eb4449
Eduard Gobjila <edikgobjila01@gmail.com>
2022-11-16 11:21:57 +0000

    added a view file for the index page also added bootstrap and font awesome

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,15 @@
 class FlatsController < ApplicationController
+
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
+  def index
+    if params[:query].present?
+      @query = params[:query]
+      @flats = Flat.where("name LIKE ?", "%#{params[:query]}%")
+      # Preventing SQL Injection and Database error for
+      # unknown characters
+    else
+      @flats = Flat.all
+    end
+  end
 end
